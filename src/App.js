eac9a8b1221cbe2bdf738e169527a7b6e6f61da3
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-30 17:24:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+const events = [];
+
+function App() {
+  return (
+    <div style={{ height: '500px' }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+}
+
+export default App;
 

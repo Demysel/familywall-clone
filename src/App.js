b39400b99b09bf6dba81f44a4d1485e840654ceb
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-30 18:28:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,11 @@ import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import TaskList from "./components/TaskList";
+import CalendarComponent from "./components/CalendarComponent";
 
 const localizer = momentLocalizer(moment);
 const events = [];
-
-function App() {
-  return (
-    <div style={{ height: '500px' }}>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
-    </div>
-  );
-}
-
-export default App;
-import React from "react";
-import TaskList from "./components/TaskList";
-import CalendarComponent from "./components/CalendarComponent";
 
 function App() {
   return (
@@ -31,9 +14,17 @@ function App() {
       <h1>Bienvenue sur FamilyWall Clone</h1>
       <TaskList />
       <CalendarComponent />
+      <div style={{ height: '500px' }}>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
+      </div>
     </div>
   );
 }
 
 export default App;
-
